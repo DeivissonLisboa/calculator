d39cbcd6eb26d52874e2b8d6a096de8db69e2df3
DeivissonLisboa <dvilaflor@gmail.com>
2022-01-13 05:09:30 -0300

    First/Completed submit

---
+++ assets/js/script.js
@@ -0,0 +1,127 @@
+// Display
+const displayText = document.getElementById("displayText");
+let displayBuffer = "";
+let operation = {};
+
+// Clear
+const clear = (clearOperation = true) => {
+    displayText.innerText = "0";
+    displayBuffer = "";
+    if (clearOperation) {
+        operation = {};
+    }
+};
+document.getElementById("clear").onclick = clear;
+
+// Change Sign
+document.getElementById("changeSign").onclick = () => {
+    const value = -parseFloat(displayText.innerText);
+    displayText.innerText = value;
+};
+
+// Percentage
+document.getElementById("porcent").onclick = () => {
+    operation.a = parseFloat(displayText.innerText);
+    operation.foo = "porcent";
+    clear(false);
+};
+
+// Division
+document.getElementById("div").onclick = () => {
+    operation.a = parseFloat(displayText.innerText);
+    operation.foo = "div";
+    clear(false);
+};
+
+// Multiplication
+document.getElementById("prod").onclick = () => {
+    operation.a = parseFloat(displayText.innerText);
+    operation.foo = "prod";
+    clear(false);
+};
+
+// Subtraction
+document.getElementById("sub").onclick = () => {
+    operation.a = parseFloat(displayText.innerText);
+    operation.foo = "sub";
+    clear(false);
+};
+
+// Sum
+document.getElementById("sum").onclick = () => {
+    operation.a = parseFloat(displayText.innerText);
+    operation.foo = "sum";
+    clear(false);
+};
+
+// Numbers
+document.getElementById("one").onclick = () => {
+    displayBuffer += 1;
+    displayText.innerText = displayBuffer;
+};
+document.getElementById("two").onclick = () => {
+    displayBuffer += 2;
+    displayText.innerText = displayBuffer;
+};
+document.getElementById("three").onclick = () => {
+    displayBuffer += 3;
+    displayText.innerText = displayBuffer;
+};
+document.getElementById("four").onclick = () => {
+    displayBuffer += 4;
+    displayText.innerText = displayBuffer;
+};
+document.getElementById("five").onclick = () => {
+    displayBuffer += 5;
+    displayText.innerText = displayBuffer;
+};
+document.getElementById("six").onclick = () => {
+    displayBuffer += 6;
+    displayText.innerText = displayBuffer;
+};
+document.getElementById("seven").onclick = () => {
+    displayBuffer += 7;
+    displayText.innerText = displayBuffer;
+};
+document.getElementById("eight").onclick = () => {
+    displayBuffer += 8;
+    displayText.innerText = displayBuffer;
+};
+document.getElementById("nine").onclick = () => {
+    displayBuffer += 9;
+    displayText.innerText = displayBuffer;
+};
+document.getElementById("zero").onclick = () => {
+    if (displayText.innerText != "0") {
+        displayBuffer += 0;
+        displayText.innerText = displayBuffer;
+    }
+};
+
+// Iguals
+document.getElementById("igual").onclick = () => {
+    const { a, foo } = operation;
+    const b = parseFloat(displayText.innerText);
+    clear();
+    if (a) {
+        switch (foo) {
+            case "sum":
+                displayText.innerText = a + b;
+                break;
+            case "sub":
+                displayText.innerText = a - b;
+                break;
+            case "prod":
+                displayText.innerText = a * b;
+                break;
+            case "div":
+                displayText.innerText = a / b;
+                break;
+            case "porcent":
+                displayText.innerText = (a / 100) * b;
+                break;
+        }
+    } else {
+        displayText.innerText = b;
+    }
+};
